e2376edf38ce8ebc27aee80fed405b4922790a81
arianabazaa <143220633+arianabazaa@users.noreply.github.com>
2026-06-22 21:25:18 -0700

    Create robot.cc

---
+++ robot.cc
@@ -0,0 +1,40 @@
+#include "robot.h"
+
+#include <string>
+
+#include "cpputils/graphics/image.h"
+
+  void Robot::SetPosition(int x, int y) {
+    x_ = x;
+    y_ = y;
+  }
+  void Robot::Draw(graphics::Image& image) {
+  // Load the image into the icon if needed.
+  if (icon1_.GetWidth() <= 0) {
+    icon1_.Load(filename1_);
+  }
+  if (icon2_.GetWidth() <= 0) {
+    icon2_.Load(filename2_);
+  }
+  mod_ = (mod_ + 1) % 2;
+  if (mod_ == 1) {
+    DrawIconOnImage(icon1_, image);
+  } else {
+    DrawIconOnImage(icon2_, image);
+  }
+}
+  void Robot::DrawIconOnImage(graphics::Image& icon,
+                            graphics::Image& image) const {
+  int width = icon.GetWidth();
+  int height = icon.GetHeight();
+  for (int i = 0; i < width; i++) {
+    for (int j = 0; j < height; j++) {
+      int x = x_ + i - width / 2;
+      int y = y_ + j - height / 2;
+      if (y >= 0 && x >= 0 && x < image.GetWidth() && y < image.GetHeight()) {
+        image.SetColor(x, y, icon.GetColor(i, j));
+      }
+    }
+  }
+
+
